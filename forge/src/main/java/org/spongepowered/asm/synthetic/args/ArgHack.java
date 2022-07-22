512cae4b28e23163df681e578b209e5be11daee1
abigailfails <thatvaliantvole@gmail.com>
2022-07-22 11:44:54 +0100

    Removed seemingly redundant arg hack mod

--- forge/src/main/java/org/spongepowered/asm/synthetic/args/ArgHack.java
+++ forge/src/main/java/org/spongepowered/asm/synthetic/args/ArgHack.java
@@ -1,11 +1,8 @@
 package org.spongepowered.asm.synthetic.args;
 
-import net.minecraftforge.fml.common.Mod;
-
 /**
  * Le funny hack to make ModifyArgs work in forge
  * @author jaacksondev
  * */
-@Mod("jacksonsmixinhackerythisshouldnotbeloaded")
 public class ArgHack {
 }
